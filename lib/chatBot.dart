be7b807fb7cab8a1a4b272103092f2143447db19
Raghavan <raghavansus@gmail.com>
2021-01-17 23:36:20 -0800

    nearbyDoc updated

---
+++ lib/chatBot.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class chatBot extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Home Page",
+      home: chatbot(),
+    );
+    // TODO: implement build
+  }
+}
+
+
+class chatbot extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new ChattsBot();
+}
+
+class ChattsBot extends State<StatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Container(
+
+      ),
+    );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'Eyes.dart';
 import 'GeneralHealth.dart';
 import 'NearbyDoc.dart';
 import 'Skin.dart';
+import 'chatBot.dart';
 
 void main() {
   runApp(mainScreen());
@@ -21,6 +22,7 @@ class mainScreen extends StatelessWidget {
         "Eyes": (BuildContext context) => new Eyes(),
         "GeneralHealth": (BuildContext context) => new GeneralHealth(),
         "Doctors Nearby": (BuildContext context) => new NearbyDoc(),
+        "Chat Bot": (BuildContext context) => new chatBot(),
       },
     );
   }
@@ -85,6 +87,14 @@ class homescreen extends State<StatefulWidget> {
               onTap: () => {
                 Navigator.push(context,
                     MaterialPageRoute(builder: (context) => NearbyDoc()))
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.local_hospital),
+              title: Text('Chat Bot'),
+              onTap: () => {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => chatBot()))
               },
             ),
           ],
@@ -171,7 +181,7 @@ class homescreen extends State<StatefulWidget> {
   }
 
   // ignore: non_constant_identifier_names
-  List<String> Categories = ["Skin", "Eyes", "GeneralHealth", "Doctors Nearby"];
+  List<String> Categories = ["Skin", "Eyes", "GeneralHealth", "Doctors Nearby", "Chat Bot"];
 
   Map jobIcon = {
     "Eyes": Icon(Icons.remove_red_eye, color: Color(0xFF083663), size: 50),
@@ -183,6 +193,8 @@ class homescreen extends State<StatefulWidget> {
     "GeneralHealth": Icon(Icons.healing, color: Color(0xFF083663), size: 50),
     "Doctors Nearby":
         Icon(Icons.local_hospital, color: Color(0xFF083663), size: 50),
+    "Chat Bot":
+    Icon(Icons.local_hospital, color: Color(0xFF083663), size: 50),
   };
 
   Widget getCategoryContainer(String categoryName) {
