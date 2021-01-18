a98b978a87d7d7503790673756d1fb385426e627
Raghavan <raghavansus@gmail.com>
2021-01-17 23:12:15 -0800

    googlemaps test

--- lib/NearbyDoc.dart
+++ lib/NearbyDoc.dart
@@ -1,49 +1,54 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'src/locations.dart' as locations;
 
-void main() => runApp(NearbyDoc());
 
-class NearbyDoc extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-
+void main() {
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<NearbyDoc> {
-  GoogleMapController mapController;
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  final LatLng _center = const LatLng(37.3230, -122.0322);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-
+class _MyAppState extends State<MyApp> {
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Maps Sample App'),
+          title: const Text('Google Office Locations'),
           backgroundColor: Colors.green[700],
         ),
         body: GoogleMap(
-
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+            target: const LatLng(0, 0),
+            zoom: 2,
           ),
+          markers: _markers.values.toSet(),
         ),
       ),
     );
   }
-
-  // Start Location Marker
-
-
-
 }
